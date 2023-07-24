3bb2b443f1e4fe12396646a0363d9dd46c82e055
47hitman <afifhabib72@gmail.com>
2023-07-24 14:36:27 +0700

    end point + custom exception

--- lib/services/endpoint.dart
+++ lib/services/endpoint.dart
@@ -1,13 +1,54 @@
-class endpoint {
-  Future bmkg() async {
-    // print(globals.token);
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'custom_exception.dart';
+
+class Endpoint {
+  String apiAddress = "https://ibnux.github.io/BMKG-importer/";
+
+//get kode wilayah
+  Future kodewilayah() async {
     final http.Response response = await http.get(
-      Uri.parse('https://ibnux.github.io/BMKG-importer/cuaca/501233.json'),
+      Uri.parse('${apiAddress}cuaca/wilayah.json'),
     );
-    print("code cuaca");
-    print(response.request);
-    print(response.body);
-    print(response);
+    // print("code cuaca");
+    // print(response.request);
+    // print(response.body);
+    // print(response);
     return _response(response);
+  }
+
+//data cuaca perwilayah
+  Future cuacawilayah(String wilayahCode) async {
+    final http.Response response = await http.get(
+      Uri.parse('${apiAddress}cuaca/$wilayahCode.json'),
+    );
+    // print("code cuaca");
+    // print(response.request);
+    // print(response.body);
+    // print(response);
+    return _response(response);
+  }
+}
+
+dynamic _response(http.Response response) {
+  switch (response.statusCode) {
+    case 200:
+      var responseJson = json.decode(response.body.toString());
+      return responseJson;
+    case 204:
+      return null;
+    case 400:
+      throw BadRequestException(response.body.toString());
+    case 401:
+      throw InvalidToken(response.body.toString());
+    case 404:
+      throw UnauthorizedException(response.body.toString());
+    case 500:
+      throw InvalidParameter(response.body.toString());
+    default:
+      throw FetchDataException(
+          'Error occured while Communication with Server with StatusCode: ${response.statusCode}');
   }
 }
